2ff87dec116e5b347a0a82e573e38547930327a7
mardsonferreira <mardsonferreira25@gmail.com>
2022-11-01 22:38:01 -0300

    Creating project

---
+++ mobile/App.tsx
@@ -0,0 +1,33 @@
+import { NativeBaseProvider, StatusBar } from "native-base";
+import {
+    useFonts,
+    Roboto_400Regular,
+    Roboto_700Bold,
+    Roboto_500Medium,
+} from "@expo-google-fonts/roboto";
+
+import { THEME } from "./src/styles/theme";
+
+import { Loading } from "./src/components/Loading";
+
+import { SignIn } from "./src/pages/SignIn";
+
+export default function App() {
+    let [fontsLoaded] = useFonts({
+        Roboto_400Regular,
+        Roboto_700Bold,
+        Roboto_500Medium,
+    });
+
+    return (
+        <NativeBaseProvider theme={THEME}>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+
+            {!fontsLoaded ? <Loading /> : <SignIn />}
+        </NativeBaseProvider>
+    );
+}
---
+++ server/src/server.ts
@@ -0,0 +1,32 @@
+import Fastify from "fastify";
+import fastifyCors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+    log: ["query"],
+});
+
+async function bootstrap() {
+    const fastify = Fastify({
+        logger: true,
+    });
+
+    await fastify.register(fastifyCors, {
+        origin: true,
+    });
+
+    fastify.get("/pools/count", async () => {
+        const count = await prisma.pool.count();
+
+        return {
+            count,
+        };
+    });
+
+    await fastify.listen({
+        port: 3333,
+        host: "0.0.0.0"
+    });
+}
+
+bootstrap();
