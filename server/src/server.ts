ab5f0c0bf2c12c190ac716808554badac699efa8
mardsonferreira <mardsonferreira25@gmail.com>
2022-11-02 17:13:59 -0300

    Creating more endpoints on server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import fastifyCors from "@fastify/cors";
+import { z } from "zod";
 import { PrismaClient } from "@prisma/client";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
     log: ["query"],
@@ -23,9 +25,53 @@ async function bootstrap() {
         };
     });
 
+    fastify.get("/users/count", async () => {
+        const count = await prisma.user.count();
+
+        return {
+            count,
+        };
+    });
+
+    fastify.get("/guesses/count", async () => {
+        const count = await prisma.guess.count();
+
+        return {
+            count,
+        };
+    });
+
+    fastify.post("/pools", async (request, reply) => {
+        const createPoolBody = z.object({
+            title: z.string(),
+        });
+
+        try {
+            const { title } = createPoolBody.parse(request.body);
+
+            const generate = new ShortUniqueId({ length: 6 });
+            const code = String(generate()).toUpperCase();
+
+            await prisma.pool.create({
+                data: {
+                    title,
+                    code,
+                },
+            });
+
+            return reply.status(201).send({
+                code,
+            });
+        } catch (error) {
+            return reply.status(403).send({
+                message: "invalid data",
+            });
+        }
+    });
+
     await fastify.listen({
         port: 3333,
-        host: "0.0.0.0"
+        host: "0.0.0.0",
     });
 }
 
